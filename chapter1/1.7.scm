7d93220eb9a6977bdc4742ada21cb5be5d741f02
Rainbowbox <twentyfivehours@163.com>
2015-12-23 22:13:32 +0800

    add 1.7

---
+++ chapter1/1.7.scm
@@ -0,0 +1,17 @@
+(define (sqrt-iter preguess guess x)
+  (if (good-enough? preguess guess x)
+    guess
+    (sqrt-iter guess (improve guess x)
+	       x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? preguess guess x)
+  (< ( / (abs (- preguess guess)) preguess ) 0.0001))
+
+(define (sqrt x)
+  (sqrt-iter 2.0 1.0 x))

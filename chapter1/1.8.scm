e6319802525f7c26cd903c7bd94bd7b5131d0bb2
Rainbowbox <twentyfivehours@163.com>
2015-12-23 22:44:27 +0800

    Add 1.8

---
+++ chapter1/1.8.scm
@@ -0,0 +1,17 @@
+(define (cube-root-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cube-root-iter (improve guess x) x)))
+
+
+(define (improve guess x)
+  (/ (+ (* 2 guess) (/ x (* guess guess))) 3))
+
+
+(define (good-enough? guess x)
+  (<= (/ (abs (- (improve guess x) guess)) guess) 0.0001))
+
+
+
+(define (cube-root x)
+  (cube-root-iter 1.0 x))

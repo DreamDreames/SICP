5bcf36feafc7d4db2b17fdc044f1d1036e775f3e
Rainbowbox <twentyfivehours@163.com>
2015-12-25 18:22:16 +0800

    Add iter version of 1.11

--- chapter1/1.11.scm
+++ chapter1/1.11.scm
@@ -1,4 +1,17 @@
 (define (f n)
   (cond ((< n 3) n)
-	(else ( + (f (- n 1)) (f (- n 2)) (f (- n 3))))))
+	(else ( + (f (- n 1)) 
+		  (* 2 (f (- n 2))) 
+		  (* 3(f (- n 3)))))))
+
+
+(define (iter-f n)
+  (define (iter a b c count)
+    (if (= count 0) 
+      	a
+	(iter b c (+ (* 3 a) (* 2 b) c) (- count 1))))
+  (iter 0 1 2 n))
+
+     
+
 

c3baf0fd669b77f3010868cd3626095bdd230ed8
Rainbowbox <twentyfivehours@163.com>
2015-12-27 22:40:49 +0800

    Add a better 1.16 solution (from SICP solutions website)

--- chapter1/1.16.scm
+++ chapter1/1.16.scm
@@ -10,4 +10,19 @@
   (cond ((= n 0) 1)
 	((even? n) (fast-expt-iter (square b) (/ n 2) 1))
 	(else ( * b (fast-expt b (- n 1))))))
+
+
+
+;; the key is do not hold any status variable in the iter function
+
+(define (fast-expt-sln b n)
+  (define (even? n)
+    (= (remainder n 2) 0))
+
+  (define (iter a b n)
+    (cond ((= n 0) a)
+	  ((even? n) (iter a (square b) (/ n 2)))
+	  (else (iter (* a b) b (- n 1)))))
+
+  (iter 1 b n))
 	

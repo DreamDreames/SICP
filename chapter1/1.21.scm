506b07502328ce81758ef4dbeeeca44065d80749
Rainbowbox <twentyfivehours@163.com>
2015-12-29 23:10:03 +0800

    Add 1.21

---
+++ chapter1/1.21.scm
@@ -0,0 +1,14 @@
+(define (smallest-divisor a)
+  (define (iter n a)
+    (cond ((>= n a) a)
+	  ((divid? n a) n)
+	  (else (iter (+ n 1) a))))
+  (define (divid? x y)
+    (= (remainder y x) 0))
+
+  (iter 2 a))
+
+(smallest-divisor 199)
+(smallest-divisor 1999)
+(smallest-divisor 19999)
+

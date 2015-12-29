da4b69a2b66c50bd06f78ee93aa532a98166e008
Rainbowbox <twentyfivehours@163.com>
2015-12-29 23:49:44 +0800

    Update 1.22 to calculate elapsed time

--- chapter1/1.22.scm
+++ chapter1/1.22.scm
@@ -1,13 +1,18 @@
-(define (timed-prime-test n)
-  (newline)
-  (display n)
-  (start-prime-test n (runtime)))
+(define (timed-search-for-primes start)
+  (define (search-for-primes start start-time) 
+    (define (even? n) 
+      (= (remainder n 2) 0))
 
-(define (start-prime-test n start-time)
-  (if (prime? n)
-    (report-prime (- (runtime) start-time))))
+    (define (report-prime p elapsed-time)
+      (display p)
+      (display " *** ")
+      (display elapsed-time))
 
-(define (report-prime elapsed-time)
-  (display " *** ")
-  (display elapsed-time))
+    (if (even? start) 
+      (search-for-primes (+ 1 start) start-time) 
+      (if (prime? start) 
+	(report-prime start (- (runtime) start-time)) 
+	(search-for-primes (+ 2 start) start-time))))
+
+  (search-for-primes start (runtime)))
 

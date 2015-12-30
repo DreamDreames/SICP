cf9aa46ba27a9e3238b25ea63bfe4fe8fc2bb7b5
Rainbowbox <twentyfivehours@163.com>
2015-12-30 23:42:17 +0800

    Update 1.22 with solutions from SICP-Solutions

--- chapter1/1.22.scm
+++ chapter1/1.22.scm
@@ -1,18 +1,35 @@
-(define (timed-search-for-primes start)
-  (define (search-for-primes start start-time) 
-    (define (even? n) 
-      (= (remainder n 2) 0))
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
 
-    (define (report-prime p elapsed-time)
-      (display p)
-      (display " *** ")
-      (display elapsed-time))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (runtime) start-time))))
 
-    (if (even? start) 
-      (search-for-primes (+ 1 start) start-time) 
-      (if (prime? start) 
-	(report-prime start (- (runtime) start-time)) 
-	(search-for-primes (+ 2 start) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
 
-  (search-for-primes start (runtime)))
 
+(define (search-for-primes start end)
+  (define (even? n)
+    (= (remainder n 2) 0))
+
+  (define (search-iter cur last)
+    (if (<= cur last) (timed-prime-test cur))
+    (if (<= cur last) (search-iter (+ cur 2) last)))
+
+  (search-iter (if (even? start) (+ start 1) start)
+	       (if (even? end) (- end 1) end)))
+
+
+(search-for-primes 1000 1019)
+(search-for-primes 10000 1037)
+(search-for-primes 100000 100043)
+(search-for-primes 1000000 1000037)
+
+(search-for-primes 1000000000 1000000021)
+(search-for-primes 10000000000 10000000061)
+(search-for-primes 100000000000 100000000057)
+(search-for-primes 1000000000000 1000000000063)

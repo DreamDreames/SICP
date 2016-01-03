f528b7265cdb0b99925ab1fd70d90b87c1d45346
Rainbowbox <twentyfivehours@163.com>
2016-01-03 22:58:11 +0800

    Add the implementation of Pi

--- chapter1/1.31.scm
+++ chapter1/1.31.scm
@@ -7,7 +7,6 @@
     (iter a 1))
 
 
-
 (define (factorial n)
   (define (inc x) (+ x 1))
 
@@ -15,5 +14,17 @@
 
   (product f 1 inc n))
 
+(define (pi n)
+  (define (inc x) (+ x 1))
+
+  (define (pi-term n)
+    (if (even? n)
+      (/ (+ n 2) (+ n 1))
+      (/ (+ n 1) (+ n 2))))
+
+  (* (product pi-term 1 inc n) 4.0))
+
 
 (factorial 3)
+
+(pi 10000)

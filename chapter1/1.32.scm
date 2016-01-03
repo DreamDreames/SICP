edd241e2ea99909114928352fb233f46873dfdd1
Rainbowbox <twentyfivehours@163.com>
2016-01-03 23:10:23 +0800

    Add an iter version of accumulate function for 1.32

--- chapter1/1.32.scm
+++ chapter1/1.32.scm
@@ -1,7 +1,13 @@
 (define (accumulate combiner null-value term a next b)
-  (if (> a b)
-    null-value
-    (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+  (define (accu-iter a result)
+    (if (> a b)
+      result
+      (accu-iter (next a) (combiner result (term a)))))
+  (accu-iter a null-value))
+
+  ;;(if (> a b)
+    ;;null-value
+    ;;(combiner (term a) (accumulate combiner null-value term (next a) next b))))
 
 
 (define (inc x) (+ x 1))

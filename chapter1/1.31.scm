6779783e31abc97f95e0fb81b49431eb0f69217d
Rainbowbox <twentyfivehours@163.com>
2016-01-03 22:50:11 +0800

    Update 1.31 to iter one

--- chapter1/1.31.scm
+++ chapter1/1.31.scm
@@ -1,7 +1,14 @@
 (define (product term a next b)
-  (if (> a b)
-    1
-    (* (term a) (product term (next a) next b))))
+  ;;(if (> a b)
+    ;;1
+    ;;(* (term a) (product term (next a) next b))))
+    (define (iter a result)
+      (if (> a b)
+	result
+	(* result (term a) (product term (next a) next b))))
+
+    (iter a 1))
+
 
 
 (define (factorial n)

ef0874f2bbdfce71f41e5c4f52f3d36d365f18e6
Rainbowbox <twentyfivehours@163.com>
2016-01-06 23:32:34 +0800

    Add an iterative version of 1.37 golden ratio

--- chapter1/1.37.scm
+++ chapter1/1.37.scm
@@ -1,9 +1,16 @@
 (define (cont-frac n d k)
-  (define (iter i)
-    (/ (n i) (iter (+ 1 i)
-  )
+  (define (iter result i)
+    (if (= i 0) 
+      result
+      (iter (/ (n i) (+ (d i) result)) (- i 1))))
 
+  (iter 0 k))
+
+
+(define k 20)
 
 (cont-frac (lambda (i) 1.0)
 	   (lambda (i) 1.0)
 	   k)
+
+

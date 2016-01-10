b3be68c6a2bd6abe6733c3374d2fb5e594ad319d
Rainbowbox <twentyfivehours@163.com>
2016-01-10 11:59:47 +0800

    Update average-damp. Add the sqrt and cube-root implementation with average-damp

--- chapter1/average-damp.scm
+++ chapter1/average-damp.scm
@@ -7,3 +7,13 @@
 
 ; 55
 ((average-damp square) 10)
+
+
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+	       1.0))
+
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+	       1.0))

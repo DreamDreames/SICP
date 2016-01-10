319e72262b0dd34c1a36a036592a32be1d97ddc5
Rainbowbox <twentyfivehours@163.com>
2016-01-10 12:10:31 +0800

    Add deriv and newton transform with deriv

---
+++ chapter1/deriv.scm
@@ -0,0 +1,23 @@
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (cube x) (* x x x))
+
+(newline)
+(display ((deriv cube) 5))
+
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newton-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt x)
+  (newton-method (lambda (y) (- (square y) x))
+		 1.0))

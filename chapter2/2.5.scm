dde91401fd4b83d40bdf945a6aad51e007394c3c
Rainbowbox <twentyfivehours@163.com>
2016-01-11 21:46:17 +0800

    Add 2.5. Implement pair structure with 2^a * 3^b

---
+++ chapter2/2.5.scm
@@ -0,0 +1,37 @@
+(define (cons a b)
+  (define (ex base p)
+    (cond ((= p 0) 1)
+	  ((= p 1) base)
+	  (else (* base (ex base (- p 1))))))
+  (* (ex 2 a) (ex 3 b)))
+
+
+(define (div num base) 
+  (cond ((= num 1) num)
+	((= (remainder num base) 0) (div (/ num base) base))
+	(else num)))
+
+(define (count-div num base)
+  (define (iter value counter)
+    (if (= value 1)
+      counter
+      (iter (/ value base) (+ counter 1))))
+  (iter num 0))
+
+(define (car x)
+  (count-div (div x 3) 2))
+
+(define (cdr x)
+  (count-div (div x 2) 3))
+
+(define n (cons 4 5))
+(newline)
+(display "car: ")
+; 4
+(display (car n))
+(newline)
+(display "cdr: ")
+;5
+(display (cdr n))
+
+

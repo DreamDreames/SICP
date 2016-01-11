26ca88fd72208560cad77c792a65e6935e273eac
Rainbowbox <twentyfivehours@163.com>
2016-01-11 23:28:32 +0800

    Implement one and two with zero and add-1

--- chapter2/2.6.scm
+++ chapter2/2.6.scm
@@ -4,6 +4,24 @@
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
 
+(define one
+  ; one is add-1 zero
+  ;(add-1 zero)
+  ; so we have
+  ;(lambda (f) (lambda (x) (f ((zero f) x))))
+  ; zero is a function that takes one arg and returns
+  ; a function that takes one arg and just returns the arg
+  ; so ((zero f) x) is just x
+  ; so we have
+  (lambda (f) (lambda (x) (f x))))
 
-(add-1 zero)
-;(display (add-1 zero))
+(define two
+  ; two is add-one one
+  ;(add-1 one)
+  ;(lambda (f) (lambda (x) (f ((one f) x)))))
+  ; replace one with its definition
+  ; we have
+  (lambda (f) (lambda (x) (f (f x)))))
+
+
+

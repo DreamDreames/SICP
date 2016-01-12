9ca22e04738e2320ef79997b902bc49c4fa3931a
Rainbowbox <twentyfivehours@163.com>
2016-01-13 00:20:28 +0800

    2.23 A for-each implementation

---
+++ chapter2/2.23.scm
@@ -0,0 +1,10 @@
+(define (for-each f items)
+  (cond ((not (null? items))
+	 (f (car items))
+	 (for-each f (cdr items)))))
+
+(for-each (lambda (x) (newline) (display x))
+	  (list 57 321 88))
+; 57
+; 321
+; 88

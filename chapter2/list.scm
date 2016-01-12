870357eb6fc3b2e4b9c33f2252327c262b28be25
Rainbowbox <twentyfivehours@163.com>
2016-01-12 22:32:14 +0800

    Update list

--- chapter2/list.scm
+++ chapter2/list.scm
@@ -9,3 +9,17 @@
 ; Scheme also provided a operator list
 ; The list above can be constructed with the following
 (list 1 2 3 4)
+
+(define one-through-four (list 1 2 3 4))
+
+one-through-four
+; (1 2 3 4)
+
+(car one-through-four)
+; 1
+
+(cdr one-through-four)
+; (2 3 4)
+
+(cons 10 one-through-four)
+; (10 1 2 3 4)

12b1e937e16ebfdde1f295c5fc9f0be188bb3954
Rainbowbox <twentyfivehours@163.com>
2016-01-12 22:39:49 +0800

    Update list with length and append operator

--- chapter2/list.scm
+++ chapter2/list.scm
@@ -23,3 +23,31 @@ one-through-four
 
 (cons 10 one-through-four)
 ; (10 1 2 3 4)
+
+(define (list-ref items n)
+  (if (= n 0)
+    (car items)
+    (list-ref (cdr items) (- n 1))))
+
+(define squares (list 1 4 9 16 25))
+(list-ref squares 3)
+; 16
+
+(define (length items)
+  (if (null? items)
+    0
+    (+ 1 (length (cdr items)))))
+
+(define odds (list 1 3 5 7))
+(length odds)
+; 4
+
+(define (append list1 list2)
+  (if (null? list1)
+    list2
+    (cons (car list1) (append (cdr list1) list2))))
+
+(append odds squares)
+; (1 3 5 7 1 4 9 16 25)
+
+

25e5f36f44c0161aae360aeb1b502974aaf991e1
Rainbowbox <twentyfivehours@163.com>
2016-01-16 23:42:29 +0800

    Use map to scale a tree

--- chapter2/scale-tree.scm
+++ chapter2/scale-tree.scm
@@ -9,3 +9,14 @@
 (display (scale-tree (list 1 (list 2 (list 3 4 (list 5) (list 6 7)))) 10))
 ; (10 (20 (30 40) 50) (60 70))
 
+
+(define (map-tree tree factor)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	   (map-tree sub-tree factor)
+	   (* sub-tree factor)))
+       tree))
+
+(newline)
+(display (map-tree (list 1 (list 2 (list 3 4 (list 5) (list 6 7)))) 10))
+; (10 (20 (30 40) 50) (60 70))

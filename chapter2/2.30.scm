09882724ca1db360d59e3f3cd76b1571faf73387
Rainbowbox <twentyfivehours@163.com>
2016-01-17 14:52:25 +0800

    2.30 square-tree without map

--- chapter2/2.30.scm
+++ chapter2/2.30.scm
@@ -5,8 +5,19 @@
 	   (* sub-tree sub-tree)))
        tree))
 
-(square-tree
-  (list 1
-	(list 2 (list 3 4) 5)
+(define (square-tree-iter tree) 
+  (cond ((null? tree) tree) 
+	((pair? tree) (cons (square-tree-iter (car tree)) (square-tree-iter (cdr tree)))) 
+	(else (* tree tree))))
+
+(define a-tree 
+  (list 1 
+	(list 2 (list 3 4) 5) 
 	(list 6 7)))
+
+(newline)
+(display (square-tree a-tree))
 ; (1 (4 (9 16) 25) (36 49))
+
+(newline)
+(display (square-tree-iter a-tree))

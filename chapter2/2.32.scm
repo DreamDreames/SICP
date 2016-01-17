c7b32de58f1ba9d26254aa4e96a87ce7b6570186
Rainbowbox <twentyfivehours@163.com>
2016-01-17 15:45:12 +0800

    2.32 all subsets of a set

---
+++ chapter2/2.32.scm
@@ -0,0 +1,13 @@
+(define nil '())
+
+(define (subsets s)
+  (if (null? s)
+    (list nil)
+    (let ((rest (subsets (cdr s))))
+      (append rest 
+	      (map (lambda (x) 
+		     (append (list (car s)) x)) 
+		   rest)))))
+
+(subsets (list 1 2 3))
+; ( () (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))

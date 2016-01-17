96e2b4891381b15d487aeab3c579780224c14f2b
Rainbowbox <twentyfivehours@163.com>
2016-01-17 21:14:29 +0800

    2.36 accumulate n

---
+++ chapter2/2.36.scm
@@ -0,0 +1,26 @@
+(define nil '())
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+	(accumulate op initial (cdr sequence)))))
+
+(define (first-dimon sequence)
+  (map (lambda (x) (car x)) sequence))
+
+(define (rest-dimon sequence)
+  (map (lambda (x) (cdr x)) sequence))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+    nil
+    (cons (accumulate op init (first-dimon seqs))
+	  (accumulate-n op init (rest-dimon seqs)))))
+
+(define seq (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+
+(newline)
+(display (accumulate-n + 0 seq))
+; (22 26 30)
+
+

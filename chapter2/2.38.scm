8f6bc096afc7e5bd979c958a430a39668330015c
Rainbowbox <twentyfivehours@163.com>
2016-01-17 21:48:32 +0800

    2.38 folder-left and folder-right

---
+++ chapter2/2.38.scm
@@ -0,0 +1,29 @@
+(define (folder-right op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+	(folder-right op initial (cdr sequence)))))
+
+(define (folder-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+      result
+      (iter (op result (car rest))
+	    (cdr rest))))
+  (iter initial sequence))
+
+(newline)
+(display (folder-right / 1 (list 1 2 3)))
+; 3/2 
+
+(newline)
+(display (folder-left / 1 (list 1 2 3)))
+; 1/6
+
+(newline)
+(display (folder-right list nil (list 1 2 3)))
+; (1 (2 (3 ()))) 
+
+(newline)
+(display (folder-left list nil (list 1 2 3)))
+; (((() 1) 2) 3)

933e586bf649fe1ac49ee9fff8c81292425fb7ca
Rainbowbox <twentyfivehours@163.com>
2016-01-20 23:59:28 +0800

    2.42 Update solution

--- chapter2/2.42.scm
+++ chapter2/2.42.scm
@@ -13,8 +13,6 @@
 (define (flatmap proc seq)
   (accumulate append nil (map proc seq)))
 
-(define empty-board nil)
-
 (define (safe? k positions)
   (define (conflict? p li)
     (= (length (filter (lambda (existing) 
@@ -27,27 +25,55 @@
 	      (conflict? cur (cdr pos))))
 	  positions))
 
-(define (adjoin-position row col rest-of-queens)
-  (map (lambda (x)
-	 (cons (cons row col) x))
-       rest-of-queens))
+(define (adjoin-position new-row k rest-of-queens)
+  (cons new-row rest-of-queens))
 
+(define empty-board nil)
+
+(define (in? x seq)
+  (cond ((null? seq) #f)
+	((= x (car seq)) #t)
+	(else (in? x (cdr seq)))))
+
+(define (same-line-safe? k positions)
+  (define (same-line-iter start seq status)
+    (if (> start k) 
+      #t
+      (cond ((in? (car seq) (cdr seq)) #f)
+	    ((= (car seq) start) (if status #f (same-line-iter (+ start 1) (cdr seq) #t)))
+	    (else (same-line-iter (+ start 1) (cdr seq) status)))))
+
+  (same-line-iter 1 positions #f))
+
+(define (diff-line-safe? k positions)
+  (define (diff-line-iter start seq)
+    (if (> start k)
+      #f
+      (if (not (in? start seq))
+	#t
+	(diff-line-iter (+ start 1) (cdr seq)))))
+  (diff-line-iter 1 positions))
+
+(define (safe? k positions)
+  (and (same-line-safe? k positions) 
+       (diff-line-safe? k positions)))
 
 (define (queens board-size)
   (define (queen-cols k)
     (if (= k 0)
-      ;(list empty-board)
-      (list )
-      ;(filter
-	;(lambda (positions) (safe? k positions))
+      (list empty-board)
+      (filter
+	(lambda (positions) (safe? k positions))
 	(flatmap
 	  (lambda (rest-of-queens)
 	    (map (lambda (new-row)
 		   (adjoin-position new-row k rest-of-queens))
 		 (enumerate-interval 1 board-size)))
 	  (queen-cols (- k 1)))
-	;)
+	)
       ))
   (queen-cols board-size))
 
-(queens 8)
+(newline)
+(define ans (queens 8))
+(ans)

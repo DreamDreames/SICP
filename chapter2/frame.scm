eaa079d97a298545be81a537dbb34bb368431d1c
Rainbowbox <twentyfivehours@163.com>
2016-01-22 15:43:18 +0800

    Update frame with segments painter

--- chapter2/frame.scm
+++ chapter2/frame.scm
@@ -10,3 +10,12 @@
 ((frame-coord-map a-frame) (make-vect 0 0))
 ;(origin-frame a-frame)
 
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+      (lambda (segment)
+	(draw-line
+	  ((frame-coord-map frame) (start-segment segment))
+	  ((frame-coord-map frame) (end-segment segment))))
+      segment-list)))
+

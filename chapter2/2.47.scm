86aa2921e68ee7885103121ee174fa0b17e4de4c
Rainbowbox <twentyfivehours@163.com>
2016-01-22 15:16:37 +0800

    2.47 define a frame

---
+++ chapter2/2.47.scm
@@ -0,0 +1,31 @@
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define origin-frame car)
+(define edge1-frame cadr)
+(define edge2-frame 
+  (lambda (f) (cadr (cdr f))))
+
+(define f (make-frame 0 1 2))
+(newline)
+(display (origin-frame f))
+(newline)
+(display (edge1-frame f))
+(newline)
+(display (edge2-frame f))
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define origin-frame car)
+(define edge1-frame cadr)
+(define edge2-frame 
+  (lambda (f) (cdr (cdr f))))
+
+(define f (make-frame 0 1 2))
+(newline)
+(display (origin-frame f))
+(newline)
+(display (edge1-frame f))
+(newline)
+(display (edge2-frame f))

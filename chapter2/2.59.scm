ba2ecd102734e2de1477c95ee1a37193366f12f9
Rainbowbox <twentyfivehours@163.com>
2016-01-29 00:38:32 +0800

    Use adjoin-set to implement union-set

--- chapter2/2.59.scm
+++ chapter2/2.59.scm
@@ -18,10 +18,10 @@
 (define (union-set set1 set2)
   (cond ((null? set1) set2)
 	((null? set2) set1)
-	((not (element-of-set? (car set1) set2))
-	 (cons (car set1) (union-set (cdr set1) set2)))
-	(else
-	  (union-set (cdr set1) set2))))
+	(else (adjoin-set (car set1) 
+			  (union-set (cdr set1) 
+				     set2)))))
+
 (newline)
 (display (union-set '(1 2) '(3)))
 ; (1 2 3)

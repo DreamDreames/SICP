0d6d188beb298115879ecc0eda35db2f50d10980
Rainbowbox <twentyfivehours@163.com>
2016-02-02 00:22:49 +0800

    2.66 lookup a key in a given tree

---
+++ chapter2/2.66.scm
@@ -0,0 +1,19 @@
+(define (make-tree entry left right)
+  (list entry left right))
+(define (entry tree)
+  (car tree))
+(define (left-branch tree)
+  (cadr tree))
+(define (right-branch tree)
+  (caddr tree))
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) 
+	 false)
+	((= given-key (entry set-of-records)) 
+	 (entry set-of-records))
+	((< given-key (entry set-of-records))
+	 (lookup given-key (left-branch set-of-records)))
+	(else
+	 (lookup given-key (right-branch set-of-records)))))
+

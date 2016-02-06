91066d7c706ce34c25bf6ff409c04241acc16d91
Rainbowbox <twentyfivehours@163.com>
2016-02-06 23:21:27 +0800

    complex-number add another layer of abstraction

--- chapter2/complex-number.scm
+++ chapter2/complex-number.scm
@@ -14,35 +14,94 @@
   (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
 		     (- (angle z1) (angle z2))))
 
-(define (real-part z) (car z))
+(define (real-part-rectangular z) (car z))
 
-(define (imag-part z) (cdr z))
+(define (imag-part-rectangular z) (cdr z))
 
-(define (magnitude z)
-  (sqrt (+ (square (real-part z)) (square (imag-part z)))))
+(define (magnitude-rectangular z)
+  (sqrt (+ (square (real-part-rectangular z)) 
+	   (square (imag-part-rectangular z)))))
 
-(define (angle z)
-  (atan (imag-part z) (real-part z)))
+(define (angle-rectangular z)
+  (atan (imag-part-rectangular z) 
+	(real-part-rectangular z)))
 
-(define (make-from-real-imag x y) (cons x y))
+(define (make-from-real-imag-rectangular x y) 
+  (attach-tag 'rectangular (cons x y)))
 
-(define (make-from-mag-ang r a)
-  (cons (* r (cons a)) (* r (sin a))))
+(define (make-from-mag-ang-rectangular r a)
+  (attach-tag 'rectangular 
+	      (cons (* r (cons a)) (* r (sin a)))))
 
 
 ; another one
+(define (real-part-polar z)
+  (* (magnitude-polar z) (cons (angle-polar z))))
+
+(define (imag-part-polar z)
+  (* (magnitude-polar z) (sin (angle-polar z))))
+
+(define (magnitude-polar z) (car z))
+
+(define (angle-polar z) (cdr z))
+
+(define (make-from-real-imag-polar x y)
+  (attach-tag 'polar 
+	      (cons (sqrt (+ (square x) (square y))) 
+		    (atan y x)))
+
+(define (make-from-mag-ang-polar r a) 
+  (attach-tag 'polar (cons r a)))
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+    (car datum)
+    (error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+    (cdr datum)
+    (error "Bad tagged datum -- CONTENTS" datum)))
+
+(define (rectangular? z)
+  (eq? (type-tag z) 'rectangular))
+
+(define (polar? z)
+  (eq? (type-tag z) 'polar))
+
 (define (real-part z)
-  (* (magnitude z) (cons (angle z))))
+  (cond ((rectangular? z)
+	 (real-part-rectangular (contents z)))
+	((polar? z)
+	 (real-part-polar (contents z)))
+	(else (error "Unknown type -- REAL-PART" z))))
 
 (define (imag-part z)
-  (* (magnitude z) (sin (angle z))))
+  (cond ((rectangular? z)
+	 (imag-part-rectangular (contents z)))
+	((polar? z)
+	 (imag-part-polar (contents z)))
+	(else (error "Unknown type -- IMAG_PART" z))))
 
-(define (magnitude z) (car z))
+(define (magnitude z)
+  (cond ((rectangular? z)
+	 (magnitude-rectangular (contents z)))
+	((polar? z)
+	 (magnitude-polar (contents z)))
+	(else (error "Unknown type -- MAGNITUDE" z))))
 
-(define (angle z) (cdr z))
+(define (angle z)
+  (cond ((rectangular? z)
+	 (angle-rectangular (contents z)))
+	((polar? z)
+	 (angle-polar (contents z)))
+	(else (error "Unknown type -- ANGLE" z))))
 
 (define (make-from-real-imag x y)
-  (cons (sqrt (+ (square x) (square y)))
-	(atan y x)))
+  (make-from-real-imag-rectangular x y))
 
-(define (make-from-mag-ang r a) (cons r a))
+(define (make-from-mag-ang r a)
+  (make-from-mag-ang-polar r a))

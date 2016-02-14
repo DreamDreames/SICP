a5caf69adc97c2f2aa0a43462445d498cbaed5ae
Rainbowbox <twentyfivehours@163.com>
2016-02-15 00:34:23 +0800

    Add some comments on the put get

--- chapter2/put-set.scm
+++ chapter2/put-set.scm
@@ -9,6 +9,13 @@
     (cond ((null? array) (list (make-entry k item)))
 	  ((equal? (key (car array)) k) array)
 	  (else (cons (car array) (put-helper k (cdr array))))))
+  ; A top-level definition
+  ; (define variable expression)
+  ; has essentially the same effect as the assignment expression, if variable is bound:
+  ; (set! variable expression) 
+  ; If variable is not bound, however, define binds variable to a new location in the current 
+  ; environment before performing the assignment (it is an error to perform a set! on an unbound 
+  ; variable)
   (set! global-array (put-helper (list op type) global-array)))
 
 (define (get op type)

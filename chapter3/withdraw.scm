fcd3cc5930ab53a42611a2df87a29db4e2c59ef2
Rainbowbox <twentyfivehours@163.com>
2016-02-20 13:33:07 +0800

    make a account package to wrap withdraw and deposit procedure

--- chapter3/withdraw.scm
+++ chapter3/withdraw.scm
@@ -36,3 +36,34 @@
 (display (w1 40))
 ; 10
 
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+	     balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+      balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+	  ((eq? m 'deposit) deposit)
+	  (else (error "Unknown request -- MAKE-ACCOUNT" m ))))
+  dispatch)
+
+(define acc (make-account 100))
+(newline)
+(display ((acc 'withdraw) 50))
+; 50
+
+(newline)
+(display ((acc 'withdraw) 60))
+; "Insufficient funds"
+((acc 'deposit) 40)
+(newline)
+(display ((acc 'withdraw) 60))
+; 30
+
+
+

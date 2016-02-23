b08411c7d6d9216c560688eb53eb45170ba871f3
Rainbowbox <twentyfivehours@163.com>
2016-02-23 14:44:04 +0800

    3.12 append! list

---
+++ chapter3/3.12.scm
@@ -0,0 +1,34 @@
+(define (append x y)
+  (if (null? x)
+    y
+    (cons (car x) (append (cdr x) y))))
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define (last-pair x)
+  (if (null? (cdr x))
+    x 
+    (last-pair (cdr x))))
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+
+(newline)
+(display (cdr x))
+; (b)
+
+(newline)
+(display (last-pair x))
+; (b)
+
+(define w (append! x y))
+(newline)
+(display w)
+; (a b c d)
+
+(newline)
+(display (cdr x))
+; (b c d)

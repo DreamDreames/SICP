9cb5cabf6101917de9587d34aa7d5398edcd2ba6
Rainbowbox <twentyfivehours@163.com>
2016-03-12 23:57:46 +0800

    update digital signal

--- chapter3/digital-signal.scm
+++ chapter3/digital-signal.scm
@@ -74,3 +74,16 @@
 
 (define (add-action! wire action-procedure)
   ((wire 'add-action!) action-procedure))
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+		  action
+		  the-agenda))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+    'done
+    (let ((first-item (first-agenda-item the-agenda)))
+      (first-item)
+      (remove-first-agenda-item! the-agenda)
+      (propagate))))

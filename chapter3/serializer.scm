ab8ec9b876f08cf6c852e60be79827b09dc6f418
Rainbowbox <twentyfivehours@163.com>
2016-03-21 21:59:18 +0800

    update serializer with test

--- chapter3/serializer.scm
+++ chapter3/serializer.scm
@@ -26,3 +26,26 @@
           val))
       serialized-p)))
 
+; test
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance) balance)
+            (else (error "Unknown request -- MAKE-ACCOUNT" m))))
+    dispatch))
+
+(define m (make-account 100))
+(newline)
+(display ((m 'withdraw) 10))
+; 90
+

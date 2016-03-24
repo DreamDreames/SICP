b841f066d4816a661194319996884a588da604a3
Rainbowbox <twentyfivehours@163.com>
2016-03-24 23:01:51 +0800

    3.56

---
+++ chapter3/3.56.scm
@@ -0,0 +1,37 @@
+(define (scale-stream s factor)
+  (cons-stream (* (stream-car s) factor)
+               (scale-stream (stream-cdr s) factor)))
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+          (let ((s1car (stream-car s1))
+                (s2car (stream-car s2)))
+            (cond ((< s1car s2car)
+                   (cons-stream s1car (merge (stream-cdr s1) s2)))
+                  ((> s1car s2car)
+                   (cons-stream s2car (merge s1 (stream-cdr s2))))
+                  (else
+                    (cons-stream s1car
+                                 (merge (stream-cdr s1)
+                                        (stream-cdr s2)))))))))
+
+(define S (cons-stream 1 (merge (scale-stream S 2) 
+                                (merge (scale-stream S 3)
+                                       (scale-stream S 5)))))
+
+(newline)
+(display (stream-ref S 0))
+(newline)
+(display (stream-ref S 1))
+(newline)
+(display (stream-ref S 2))
+(newline)
+(display (stream-ref S 3))
+(newline)
+(display (stream-ref S 4))
+(newline)
+(display (stream-ref S 5))
+(newline)
+(display (stream-ref S 6))

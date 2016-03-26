3a6a3b64d9af2fb970f6521d1d9a5d1ffbc4ddd5
Rainbowbox <twentyfivehours@163.com>
2016-03-26 17:55:44 +0800

    stream model in sqrt calculation

---
+++ chapter3/stream-calculation.scm
@@ -0,0 +1,24 @@
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 1.0
+                 (stream-map (lambda (guess)
+                               (sqrt-improve guess x))
+                             guesses)))
+  guesses)
+
+(define (display-stream s)
+  (if (stream-null? s)
+    '()
+    (begin
+      (newline)
+      (display (stream-car s))
+      (display-stream (stream-cdr s)))))
+
+(display-stream (sqrt-stream 2))
+

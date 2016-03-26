bf6d651a6a790aa3622823947ec4ae03d959944c
Rainbowbox <twentyfivehours@163.com>
2016-03-26 18:04:33 +0800

    calcule Pi using stream model

--- chapter3/stream-calculation.scm
+++ chapter3/stream-calculation.scm
@@ -20,5 +20,21 @@
       (display (stream-car s))
       (display-stream (stream-cdr s)))))
 
-(display-stream (sqrt-stream 2))
+;(display-stream (sqrt-stream 2))
 
+(define (partial-sums s)
+  (cons-stream (stream-car s)
+               (stream-map + (partial-sums s) (stream-cdr s))))
+
+(define (scale-stream s factor)
+  (cons-stream (* (stream-car s) factor)
+               (scale-stream (stream-cdr s) factor)))
+
+
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (pi-summands (+ n 2)))))
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+(display-stream pi-stream)

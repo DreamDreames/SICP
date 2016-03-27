74077db6b732091d1c7794175013955679308ecd
Rainbowbox <twentyfivehours@163.com>
2016-03-27 20:01:47 +0800

    3.64 update stream limit with stream-ref

--- chapter3/3.64.scm
+++ chapter3/3.64.scm
@@ -20,15 +20,24 @@
       (display (stream-car s))
       (display-stream (stream-cdr s)))))
 
-;(display-stream (sqrt-stream 2))
-
 (define (stream-limit s tolerance)
   (if (< (abs (- (stream-car s) (stream-car (stream-cdr s)))) tolerance)
     (stream-car s)
     (stream-limit (stream-cdr s) tolerance)))
 
+(define (stream-limit2 s tolerance)
+  (if (< (abs (- (stream-ref s 0) (stream-ref s 1))) tolerance)
+    (stream-ref s 0)
+    (stream-limit (stream-cdr s) tolerance)))
+
 (define (sqrt x tolerance)
   (stream-limit (sqrt-stream x) tolerance))
 
+(define (sqrt2 x tolerance)
+  (stream-limit2 (sqrt-stream x) tolerance))
+
 (newline)
 (display (sqrt 2 0.000001))
+
+(newline)
+(display (sqrt2 2 0.000001))

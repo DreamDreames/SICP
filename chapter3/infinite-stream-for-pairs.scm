eb19bb3f35de48d9ba6356aecef930b111bca040
Rainbowbox <twentyfivehours@163.com>
2016-03-27 20:53:14 +0800

    pairs as infinite stream

---
+++ chapter3/infinite-stream-for-pairs.scm
@@ -0,0 +1,29 @@
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+    s2
+    (cons-stream (stream s1)
+                 (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (cons-stream 
+    (list (stream-car s) (stream-car t))
+    (interleave 
+      (stream-map (lambda (x) (list (stream-car s) x))
+                  (stream-cdr t))
+      (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (integers-start-from n)
+  (cons-stream n
+               (integers-start-from (+ n 1))))
+
+(define integer
+  (integers-start-from 0))
+
+(define (display-stream s)
+  (newline)
+  (display (stream-car s))
+  (display-stream (stream-cdr s)))
+
+(define test (stream-ref (pairs integer integer) 10))
+(newline)
+(display test)

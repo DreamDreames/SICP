b3c8c6061cf951deb7f668887c9732ed5db5ff01
Rainbowbox <twentyfivehours@163.com>
2016-03-30 23:35:38 +0800

    Update eval with apply

--- chapter4/eval.scm
+++ chapter4/eval.scm
@@ -17,3 +17,17 @@
                 (list-of-values (operands exp) env)))
         (else
           (error "Unknown expression type -- EVAL" exp))))
+
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+           (procedure-body procedure)
+           (extend-evnironment
+             (procedure-parameters procedure)
+             arguments
+             (procedure-environment procedure))))
+        (else
+          (error
+            "Unknown procedure type -- APPLY" procedure))))

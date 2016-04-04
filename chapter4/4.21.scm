ca0ac2c448ed3c4c5f72d66807a38c19820ee9a3
Rainbowbox <twentyfivehours@163.com>
2016-04-04 23:53:53 +0800

    4.21 recursive with lambda only

--- chapter4/4.21.scm
+++ chapter4/4.21.scm
@@ -1,6 +1,10 @@
 ((lambda (n)
    ((lambda (fact)
       (fact fact n))
+    ; the first arg ft is necessary 
+    ; because this lambda needs to call itself recursively
+    ; if there's no ft, it cannot reference to itself
+    ; so in fact, ft is this lambda itself
     (lambda (ft k)
       (if (= k 1)
         1 
@@ -23,9 +27,14 @@
 (define (f x)
   ((lambda (new-even? new-odd?)
      (new-even? new-even? new-odd? x))
+   ; in this case, ev? is the 2nd lambda
+   ; od? is the 3rd lambda
+   ; actually we are using the parameter of lambda to refer to lambda
    (lambda (ev? od? n)
      (if (= n 0) true (od? ev? od? (- n 1))))
    (lambda (ev? od? n)
      (if (= n 0) false (ev? ev? od? (- n 1))))))
-
+; is even ?
+; (f 10) -> #t
+; (f 3) -> #f
 

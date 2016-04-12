362133e6f85ea17995692ff7e426085343835e51
Rainbowbox <twentyfivehours@163.com>
2016-04-12 23:47:46 +0800

    4.35 amb integer between low and high

---
+++ chapter4/4.35.scm
@@ -0,0 +1,10 @@
+(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high)))
+    (let ((j (an-integer-between i high)))
+      (let ((k (an-integer-between j high)))
+        (require (= (+ (* i i) (* j j)) (* k k)))
+        (list i j k)))))
+
+(define (an-integer-between low high)
+  (amb low (an-integer-between (+ low 1) high)))
+
